e66e09a99546c694e8e1d0c474f451ac101465c4
Bicer <Bicer@RYUK>
2026-06-03 10:05:47 +0300

    Proje dosyası ekle.

---
+++ 1-10_arasi_2_ve_3_bolunen_sayilar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_10_arasi_2_ve_3_bolunen_sayilar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string sonuc = "";
+
+                for ( int i = 1; i <= 10; i++)
+                {
+                    if ( (i % 2 == 0) && (i % 3 == 0) )
+                    {
+                        sonuc += i + " ";
+                    }
+                }
+
+                Console.WriteLine(" 1 ile 10 arasındaki ( 1 ve 10 dahil ) sayılardan hem 2'ye hem 3'e bölünen sayılar : {0} ", sonuc);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 1-10_arasi_2_ve_3_bolunen_sayilar/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("1-10_arasi_2_ve_3_bolunen_sayilar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1-10_arasi_2_ve_3_bolunen_sayilar")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("9f29d759-daeb-4a29-8d53-b9fd92023579")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
